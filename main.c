459c80550155b717a7551fd61f97791561908f39
mohsen <mohsenaminjavaheri@gmail.com>
2021-11-10 13:33:25 +0330

    start code

--- main.c
+++ main.c
@@ -14,7 +14,37 @@
 
 int main(void)
 {
+	DelayInit();
 	
+	Config_GPIO(GPIOB,GPIO_Pin_15,GPIO_Mode_Out_PP);	
+
+	Config_GPIO(GPIOA,GPIO_Pin_8,GPIO_Mode_Out_PP);	
+
+	Config_GPIO(GPIOB,GPIO_Pin_9,GPIO_Mode_Out_PP);	
+
+	Config_GPIO(GPIOC,GPIO_Pin_13,GPIO_Mode_Out_PP);	
+	
+	for(int i=0;i<=4;i++)
+	{
+		GPIOB->BSRR=GPIO_Pin_9;
+		DelayMs(200);
+		GPIOC->BSRR=GPIO_Pin_13;
+		DelayMs(200);
+		GPIOB->BSRR=GPIO_Pin_15;
+		DelayMs(200);
+		GPIOA->BSRR=GPIO_Pin_8;
+		DelayMs(200);
+		
+		GPIOB->BRR=GPIO_Pin_9;
+		DelayMs(200);
+		GPIOC->BRR=GPIO_Pin_13;
+		DelayMs(200);
+		GPIOB->BRR=GPIO_Pin_15;
+		DelayMs(200);
+		GPIOA->BRR=GPIO_Pin_8;
+		DelayMs(200);
+		
+	}
 	
 	while(1)
 	{
