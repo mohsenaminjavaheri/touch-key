5ab1dcc820b978b23510d99f23bf92aea5c6b94b
mohsen <mohsenaminjavaheri@gmail.com>
2021-11-20 09:34:48 +0330

    c

--- main.c
+++ main.c
@@ -14,27 +14,6 @@
 
 int main(void)
 {
-	DelayInit();
-	
-	lcd16x2_init(LCD16X2_DISPLAY_ON_CURSOR_OFF_BLINK_OFF);
-	
-	lcd16x2_gotoxy(0,0);
-		
-	lcd16x2_puts("   Hello HoE");
-	
-	DelayMs(500);
-	
-	lcd16x2_clrscr();
-		GPIOB->BRR=GPIO_Pin_9;
-		DelayMs(200);
-		GPIOC->BRR=GPIO_Pin_13;
-		DelayMs(200);
-		GPIOB->BRR=GPIO_Pin_15;
-		DelayMs(200);
-		GPIOA->BRR=GPIO_Pin_8;
-		DelayMs(200);
-		
-		
 
 	
 	while(1)
