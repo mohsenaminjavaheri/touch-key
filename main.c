c4ca949a8b9e7c992fa28e6f72c3d9b2e680e065
mohsen <mohsenaminjavaheri@gmail.com>
2021-11-20 09:57:55 +0330

    start in config

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 
 #include "delay.h"
 #include "gpio.h"
+#include "lcd16_2.h"
 
 
 
@@ -24,29 +25,35 @@ int main(void)
 
 	Config_GPIO(GPIOC,GPIO_Pin_13,GPIO_Mode_Out_PP);	
 	
-	for(int i=0;i<=4;i++)
+	lcd16x2_init(LCD16X2_DISPLAY_ON_CURSOR_OFF_BLINK_OFF);
+	
+	lcd16x2_gotoxy(0,0);
+		
+	lcd16x2_puts(" please wait...");
+	
+	for(int i=0;i<3;i++)
 	{
-		GPIOB->BSRR=GPIO_Pin_9;
-		DelayMs(200);
 		GPIOC->BSRR=GPIO_Pin_13;
+		DelayMs(200);
+		GPIOB->BSRR=GPIO_Pin_9;
 		DelayMs(200);
 		GPIOB->BSRR=GPIO_Pin_15;
 		DelayMs(200);
 		GPIOA->BSRR=GPIO_Pin_8;
 		DelayMs(200);
 		
-		GPIOB->BRR=GPIO_Pin_9;
-		DelayMs(200);
 		GPIOC->BRR=GPIO_Pin_13;
+		DelayMs(200);
+		GPIOB->BRR=GPIO_Pin_9;
 		DelayMs(200);
 		GPIOB->BRR=GPIO_Pin_15;
 		DelayMs(200);
 		GPIOA->BRR=GPIO_Pin_8;
-		DelayMs(200);
-		
-		
-		
+		DelayMs(200);		
 	}
+	lcd16x2_clrscr();
+	
+	lcd16x2_puts("  ***Hi HoE***");
 	
 	while(1)
 	{
